9fa83b8c2f12c3bf71100accf6799b813df89aeb
al-chaudhari <chaudharia041@gmail.com>
2021-05-17 19:01:38 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Swiss Factur X E Invoicing
+
+A E-Invoice PDF Creation library
+
+#### License
+
+MIT
---
+++ swiss_factur_x_e_invoicing/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/swiss_factur_x_e_invoicing"
+# docs_base_url = "https://[org_name].github.io/swiss_factur_x_e_invoicing"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Swiss Factur X E Invoicing"
