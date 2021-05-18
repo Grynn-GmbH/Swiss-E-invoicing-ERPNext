dce696f5170b3409f08318153e4bbbbb2c782377
al-chaudhari <chaudharia041@gmail.com>
2021-05-18 19:35:21 +0530

    added base html

--- swiss_factur_x_e_invoicing/attach_e_pdf.py
+++ swiss_factur_x_e_invoicing/attach_e_pdf.py
@@ -1,6 +1,11 @@
 # Facturx
+import frappe
+from frappe import _
 from facturx import generate_facturx_from_file
+import html
 
 
-def attach_e_pdf():
+def attach_e_pdf(doc, events=None):
+    company = frappe.get_doc('Company', doc.company)
+    print(company.terms)
     pass
