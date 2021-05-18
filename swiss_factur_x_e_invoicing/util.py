3f210c6e3a184c05e6a2c7e517207120e4eb9234
al-chaudhari <chaudharia041@gmail.com>
2021-05-18 21:23:34 +0530

    added get_xml_path()

--- swiss_factur_x_e_invoicing/util.py
+++ swiss_factur_x_e_invoicing/util.py
@@ -1,4 +1,5 @@
 import frappe
+from os import path,
 
 
 def get_pdf_data(doctype, name):
@@ -7,3 +8,7 @@ def get_pdf_data(doctype, name):
     """
     html = frappe.get_print(doctype, name)
     return frappe.utils.pdf.get_pdf(html)
+
+
+def get_xml_path():
+    app_dir = path.dirname(path.realpath(__file__))
