ba9efd2e6ebcc735ccbef06e0a65f53c36800823
al-chaudhari <chaudharia041@gmail.com>
2021-05-18 21:20:52 +0530

    update doc

--- swiss_factur_x_e_invoicing/util.py
+++ swiss_factur_x_e_invoicing/util.py
@@ -1,8 +1,9 @@
 import frappe
 
+
 def get_pdf_data(doctype, name):
     """
-    Get Print of Pdf Data
+    Get Print from Default Template
     """
     html = frappe.get_print(doctype, name)
     return frappe.utils.pdf.get_pdf(html)
